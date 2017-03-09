00ebcfb950069dffc56134c83de37da85f161486
DanielECLab <dperibanez@elconfidencial.com>
2017-03-09 08:57:52 +0100

    First commit

---
+++ bikeModel.js
@@ -0,0 +1,12 @@
+//Main JS
+
+function BikeManager(){
+
+    this.initialize = function() {
+        //TODO: load the Model
+    }
+
+    this.getStationById = function(stationId){
+        //TODO: return the proper station
+    }
+}
